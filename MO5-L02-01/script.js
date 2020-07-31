024525ec5885041383ef022fcae16f40d7a8ca77
Eefjeslierendrecht <64635639+Eefjeslierendrecht@users.noreply.github.com>
2020-07-31 11:27:24 +0200

    Add files via upload

---
+++ MO5-L02-01/script.js
@@ -0,0 +1,11 @@
+console.log('Mijn script werkt!')
+
+
+console.log("He hallo, ik ben eerste")
+setTimeout(function () { console.log("He hallo, ik ben tweede"); }, 2000);
+console.log("He hallo, ik ben derde")
+
+// verwachte uitkomst van bovenstaande code:
+// He halllo, ik ben de eerste
+// He hallo, ik ben de derde
+// He hallo ik ben de tweede
